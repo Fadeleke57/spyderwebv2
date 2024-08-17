b6121aaf06b58733d79da4485addcea43c77db28
fadeleke57@gmail.com <fadeleke57@gmail.com>
2024-08-17 16:13:56 -0400

    update to start sh

--- backend/start.sh
+++ backend/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd "$(dirname "$0")"
-pip install -r requirements.txt
+pip install -r requirements.txt --break-system-packages
 python3 -m uvicorn src.main:app --reload --workers 1 --host 0.0.0.0 --port 8000
