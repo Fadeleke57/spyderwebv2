0f95866633d8bbfc6845c4efc4389c39465e5942
fadeleke57@gmail.com <fadeleke57@gmail.com>
2024-08-17 17:16:57 -0400

    ec2 mongo package

--- backend/start.sh
+++ backend/start.sh
@@ -6,5 +6,6 @@ echo "Pip packages:"
 pip show fastapi_login
 
 cd "$(dirname "$0")"
+source venv/bin/activate
 pip install -r requirements.txt --break-system-packages
 python3 -m uvicorn src.main:app --reload --workers 1 --host 0.0.0.0 --port 8000
