79c45aaac64282b7368c99701da23c9389957a6b
fadeleke57@gmail.com <fadeleke57@gmail.com>
2024-08-18 12:13:17 -0400

    use user to auth components instead of token

--- frontend/src/hoc/withAuth.tsx
+++ frontend/src/hoc/withAuth.tsx
@@ -1,20 +1,22 @@
 import { useSession, signIn } from "next-auth/react";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import { useUser } from "@/context/UserContext";
 
 export default function withAuth(Component: any) {
   return function AuthenticatedComponent(props: any) {
     const { data: session, status } = useSession();
+    const { user } = useUser();
     const loading = status === "loading";
     const router = useRouter();
 
     useEffect(() => {
       const token = localStorage.getItem("token");
 
-      if (!token) {
+      if (!user) {
         router.push("/auth/login");
       }
-    }, [router]);
+    }, [user, router]);
 
 
     return <Component {...props} />;
