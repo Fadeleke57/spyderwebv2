c2f5322aac6610447e1e54787285083a80b09ecf
Farouk Adeleke <fadel@Farouks-MacBook-Air.local>
2024-08-29 04:21:59 +0100

    start script update

--- backend/start.sh
+++ backend/start.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-echo "Using Python: $(which python3)"
-echo "Python version: $(python3 --version)"
-echo "Pip packages:"
-pip show fastapi_login
-
 cd "$(dirname "$0")"
-source venv/bin/activate
-pip install -r requirements.txt
+source env/bin/activate
+pip3 install -r requirements.txt
 python3 -m uvicorn src.main:app --reload --workers 1 --host 0.0.0.0 --port 8000
