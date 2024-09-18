056cc411ca54d36f3885b4762c849057220db0ad
Farouk Adeleke <fadel@Farouks-MacBook-Air.local>
2024-09-18 11:54:28 -0400

    feat: about page

--- frontend/src/components/landing/Demo.tsx
+++ frontend/src/components/landing/Demo.tsx
@@ -4,8 +4,8 @@ import { ChartBarBig } from "lucide-react";
 
 export default function Demo() {
   return (
-    <div className="relative mt-32 lg:mt-20 flex flex-col gap-10 lg:flex-row rounded-xl lg:bg-muted lg:p-10 ">
-      <div className="pt-0 lg:px-0 lg:pt-0">
+    <div className="relative mt-32 lg:mt-20 flex flex-col gap-10 lg:gap-0 lg:flex-row rounded-xl lg:bg-muted lg:p-10 ">
+      <div className="lg:px-0 h-fit lg:border-b-2 lg:border-slate-300 lg:pb-4">
         <h1 className="text-3xl sm:text-4xl lg:text-4xl font-bold tracking-tight mb-2 sm:mb-4 max-w-7xl mx-auto">
           An <span className="text-blue-500">all-in-one</span> research tool for
           understanding the full context of your news.
@@ -14,7 +14,10 @@ export default function Demo() {
           Visualize connections between articles, track bias, and conduct deep,
           interactive research that goes beyond simple summaries.
         </p>
-        <Button className="mt-6" variant={"outline"}><ChartBarBig className="mr-2"/>Why this matters</Button>
+        <Button className="mt-6" variant={"outline"}>
+          <ChartBarBig className="mr-2" />
+          Why this matters
+        </Button>
       </div>
       <TerminalDemo />
     </div>
--- frontend/src/components/utility/Loading.tsx
+++ frontend/src/components/utility/Loading.tsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 function LoadingPage() {
   useEffect(() => {
     const loadQuantum = async () => {
-      const { quantum } = await import('ldrs');
+      const { quantum } = await import("ldrs");
       quantum.register();
     };
 
@@ -13,10 +13,26 @@ function LoadingPage() {
   }, []);
 
   return (
-    <div className="w-full h-full flex justify-center items-center z-50">
+    <div className="w-screen h-screen flex justify-center items-center z-50">
       <l-quantum size="150" speed="2.4" color="black"></l-quantum>
     </div>
   );
 }
+
+export const LandingLoader = () => {
+  useEffect(() => {
+    const loadGrid = async () => {
+      const { grid } = await import("ldrs"); 
+      console.log("grid", grid);
+      grid.register();
+    };
+    loadGrid();
+  });
+  return (
+    <div className="w-full h-full flex justify-center items-center z-50">
+      <l-grid size="60" speed="1.5" color="black"></l-grid>
+    </div>
+  );
+};
 
 export { LoadingPage };
