45ca085ebef449fc24ee65d823072903b9071efb
Farouk Adeleke <fadeleke57@gmail.com>
2024-12-22 00:20:41 -0500

    remove article id reference

--- frontend/src/components/buckets/BucketChart.tsx
+++ frontend/src/components/buckets/BucketChart.tsx
@@ -7,7 +7,7 @@ function BucketChart({ bucket }: { bucket: Bucket }) {
     <CardContent className="grid gap-4 p-0 py-4 px-0">
       <div className="grid auto-rows-min gap-2">
         <div className="flex items-baseline gap-1 text-2xl font-bold tabular-nums leading-none">
-          {bucket.articleIds.length}
+          N/A
           <span className="text-sm font-normal text-muted-foreground">
             Sources Added
           </span>
