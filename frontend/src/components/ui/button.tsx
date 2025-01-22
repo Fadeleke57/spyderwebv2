0d1c191811ee7494748f0d1532b1e17523cab92f
Farouk Adeleke <fadeleke57@gmail.com>
2025-01-22 12:00:50 +0000

    feat: support for user bucket search

--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -17,7 +17,7 @@ const buttonVariants = cva(
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground dark:hover:bg-muted",
         secondary:
           "bg-blue-400 text-background dark:text-secondary-foreground hover:bg-blue-500",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
+        ghost: "hover:bg-muted hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
--- frontend/src/hooks/general.ts
+++ frontend/src/hooks/general.ts
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { RefObject } from "react";
 
 function useMediaQuery(query: string): boolean {
   const [matches, setMatches] = useState<boolean>(() => {
@@ -16,6 +17,33 @@ function useMediaQuery(query: string): boolean {
   }, [query]);
 
   return matches;
+}
+
+type Event = MouseEvent | TouchEvent;
+
+export function useOnClickOutside<T extends HTMLElement = HTMLElement>(
+  ref: RefObject<T>,
+  handler: (event: Event) => void,
+  mouseEvent: "mousedown" | "mouseup" = "mousedown"
+): void {
+  useEffect(() => {
+    const listener = (event: Event) => {
+      const el = ref?.current;
+      if (!el || el.contains((event?.target as Node) || null)) {
+        return;
+      }
+
+      handler(event);
+    };
+
+    document.addEventListener(mouseEvent, listener);
+    document.addEventListener("touchend", listener);
+
+    return () => {
+      document.removeEventListener(mouseEvent, listener);
+      document.removeEventListener("touchend", listener);
+    };
+  }, [ref, handler, mouseEvent]);
 }
 
 export default useMediaQuery;
