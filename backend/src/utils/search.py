d2aeb48c00c4e65d2320e5130bf37419c1b7de62
Farouk Adeleke <fadeleke57@gmail.com>
2025-03-10 13:55:12 +0000

    add namespace 'webs'

--- backend/src/utils/search.py
+++ backend/src/utils/search.py
@@ -8,6 +8,7 @@ def run_semantic_search(query: str, limit: int, filter):
         vector=query_embedding,
         top_k=limit,
         include_metadata=True,
+        namespace="webs",
         filter=filter,
     )
     results = []
