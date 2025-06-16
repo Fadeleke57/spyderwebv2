301f4415dd52de025bf7c6fe76ab13f2810c83f8
Farouk Adeleke <fadeleke57@gmail.com>
2025-06-16 17:55:09 -0400

    remove unused imports

--- frontend/src/components/ui/sidebar.tsx
+++ frontend/src/components/ui/sidebar.tsx
@@ -4,8 +4,6 @@ import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { VariantProps, cva } from "class-variance-authority";
 import {
-  ArrowLeftFromLine,
-  ArrowRightFromLine,
   PanelLeft,
   PanelRight,
 } from "lucide-react";
